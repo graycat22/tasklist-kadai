7acc6877e094f369de9e00ee5483c18980b1ae44
Kame Asari <asaneko22@gmail.com>
2022-02-14 02:10:09 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
-  def index # index は目次なので一覧表示にする必要がある
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
+
+  def index
     @tasks = Task.all
   end
   
   def show
-    @task = Task.find(params[:id])
   end
   
   def new
@@ -23,11 +24,9 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     if @task.update(task_params)
       flash[:success] = "タスクは更新されました"
       redirect_to @task
@@ -38,13 +37,16 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     flash[:success] = "タスクは削除されました"
     redirect_to tasks_url
   end
   
   private
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
   def task_params
     params.require(:task).permit(:content)
   end
