a047129faa6fac54afc59c407eb2c6294704115c
graycat22 <asaneko22@gmail.com>
2022-03-02 16:06:03 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,22 +1,26 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:destroy]
+  before_action :require_user_logged_in
 
   def index
-    @tasks = Task.all
+    if logged_in?
+      @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
+    end
   end
   
   def show
   end
   
   def new
-    @task = Task.new
+    @task = current_user.tasks.build
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = "タスクが追加されました"
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = "タスクが追加されませんでした"
       render :new
@@ -39,16 +43,23 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     flash[:success] = "タスクは削除されました"
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
   end
   
   private
   
   def set_task
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
 
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
