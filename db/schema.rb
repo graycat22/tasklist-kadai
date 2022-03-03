0f0d597ad91e5c5d76aa3195f84601b5a4ce92ec
kame.andy <asaneko22@gmail.com>
2022-03-03 06:28:44 +0000

    resubmit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_02_152810) do
+ActiveRecord::Schema.define(version: 2022_03_03_040700) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
